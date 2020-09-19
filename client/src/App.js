8932678279f9e509198992b466b6feb8c6afa8d4
Mitchell <mitchell.rian.smith@gmail.com>
2020-09-19 11:26:02 -0700

    Move socket code from App to ClientSocket, and render the ClientSocket in the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
+import React from "react";
+import ClientSocket from './components/ClientSocket.js';
+
 //Dev notes about this endpoint url
 //// If changing to port 5000 the backend will see the request sent every second, but the frontend wil not get a response.
 //// If changing to add the socket path, :5001/socket, the backend will receive one connection but will not respond.
-const ENDPOINT = "http://127.0.0.1:5001";
+
 
 function App() {
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", data => {
-      setResponse(data);
-    });
-  }, []);
-
+  
   return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
+      <div>
+        <ClientSocket />
+      </div>
   );
 }
 
